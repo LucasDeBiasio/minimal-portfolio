e321b3c7ec6cf21a2a7a28eb3c80fbf19165b4f1
Lucas De Biasio <lucasnoeldeb@gmail.com>
2023-09-15 22:10:28 -0700

    added some simple words and styling to about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 function About() {
     return (
-        <div>About</div>
+        <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
+            <h1 className='text-2xl md:text-3xl mb-1 md:mb-3 font-semibold'>About</h1>
+            <p className='text-sm max-w-xl mb-6 font-semibold'>stuff
+            <br/>
+            more stuff
+            <br/>
+            more stuff
+            </p>
+        </div>
     )
 }
 
