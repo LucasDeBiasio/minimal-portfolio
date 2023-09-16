b5bd0ffac609ee503405f6d87766dd37a8365bf5
Lucas De Biasio <lucasnoeldeb@gmail.com>
2023-09-15 22:14:46 -0700

    added more project placeholders

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -17,4 +17,22 @@ export default [
         stack: ['n/a'],
         link: '',
     },
+    {
+        title: 'Empty',
+        imgUrl: '/assets/empty-box.jpg',
+        stack: ['n/a'],
+        link: '',
+    },
+    {
+        title: 'Empty',
+        imgUrl: '/assets/empty-box.jpg',
+        stack: ['n/a'],
+        link: '',
+    },
+    {
+        title: 'Empty',
+        imgUrl: '/assets/empty-box.jpg',
+        stack: ['n/a'],
+        link: '',
+    },
 ]
